f98719283a39065d90a9ba89f7f948890bc8dd11
Hans Scholze <hans.scholze@gmail.com>
2015-04-22 22:51:34 -0700

    tag merge points with None using the same dict as the rest of the paths, factor out directory recursion into separate function, seeems to work

--- stackdir.py
+++ stackdir.py
@@ -3,43 +3,49 @@ from collections import Counter
 import stat
 import pathlib as pl
 
-abspath = pl.Path('/home/hans/utest')
-roots = [abspath / pl.Path(r) for r in ('a', 'b', 'c')]
-
-paths = dict(((p.relative_to(roots[0]), 0) for p in roots[0].iterdir()))
-
-merges = set()
 
 def is_dir(p):
     return stat.S_ISDIR(p.lstat().st_mode)
 
-def add_path(roots, paths, merges, new_root_i, new_path):
+
+def add_path(paths, roots, new_root_i, new_path):
     new_root = roots[new_root_i]
     path_rel = new_path.relative_to(new_root)
-    try:
-        old_root_i = paths.pop(path_rel)
-    except KeyError:
-        paths[path_rel] = new_root_i
-    else:
-        old_root = roots[old_root_i]
-        old_path = old_root / path_rel
-        if is_dir(old_path) and is_dir(new_path):
-            merges.add(path_rel)
-            for child_path in old_path.iterdir():
-                child_path_rel = child_path.relative_to(old_root)
-                assert child_path_rel not in paths
-                paths[child_path_rel] = old_root_i
-            for child_path in new_path.iterdir():
-                add_path(roots, paths, merges, new_root_i, child_path)
+    if path_rel in paths:
+        old_root_i = paths[path_rel]
+        if old_root_i is None:
+            add_dir(paths, roots, new_root_i, new_path)
         else:
-            print('warning: {} shadows {}'.format(new_path, old_path))
-            paths[path_rel] = new_root_i
+            old_root = roots[old_root_i]
+            old_path = old_root / path_rel
+            if is_dir(old_path) and is_dir(new_path):
+                paths[path_rel] = None
+                add_dir(paths, roots, old_root_i, old_path)
+                add_dir(paths, roots, new_root_i, new_path)
+            else:
+                print('warning: "{}" shadows "{}"'.format(new_path, old_path))
+                paths[path_rel] = new_root_i
+    else:
+        paths[path_rel] = new_root_i
 
 
+def add_dir(paths, roots, root_i, path):
+    assert is_dir(path)
+    for child_path in path.iterdir():
+        add_path(paths, roots, root_i, child_path)
 
-def add(new_root_i, new_path):
-    add_path(roots, paths, merges, new_root_i, new_path)
 
-x=list(roots[1].iterdir())
-add(1, x[0])
+def add_roots(paths, roots):
+    for i, root in enumerate(roots):
+        add_dir(paths, roots, i, root)
+
+
+abspath = pl.Path('/home/hans/utest')
+roots = [abspath / pl.Path(r) for r in ('a', 'b', 'c')]
+for r in roots: assert is_dir(r)
+
+paths = {}
+
+add_roots(paths, roots)
+
 
